1b7668ceb8be97c546223e9cb9463f4a61d252f6
Sárándi <sarandi.balint@gmail.com>
2022-09-10 11:41:17 +0200

    Task 13 done

--- src/hu/petrik/java02ora/Feladat13.java
+++ src/hu/petrik/java02ora/Feladat13.java
@@ -1,4 +1,28 @@
 package hu.petrik.java02ora;
 
+import java.util.Scanner;
+
+import static java.lang.System.out;
+import static java.lang.System.in;
+
 public class Feladat13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(in);
+        int gondoltszam = (int)(Math.random() * 100) + 1;
+        out.print("Adjon meg egy pozitív egész számot 1 és 100 között: ");
+        int tipp = sc.nextInt();
+        int kulombseg;
+        do {
+            if (gondoltszam > tipp){
+                kulombseg = gondoltszam-tipp;
+                out.printf("A gondolt szám %s-al nagyobb!\n",kulombseg);
+            } else if (gondoltszam < tipp) {
+                kulombseg = tipp - gondoltszam;
+                out.printf("A gondolt szám %s-el kisebb!\n",kulombseg);
+            }
+            out.print("Adjon meg egy pozitív egész számot 1 és 100 között: ");
+            tipp = sc.nextInt();
+        }while(gondoltszam != tipp);
+        out.println("Gratulálok, eltalálta!");
+    }
 }
