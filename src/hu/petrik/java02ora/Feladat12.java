b98718c73519ce75c82eecf5570e38fc1483074a
Sárándi <sarandi.balint@gmail.com>
2022-09-10 11:26:15 +0200

    Task 1-12 done

--- src/hu/petrik/java02ora/Feladat12.java
+++ src/hu/petrik/java02ora/Feladat12.java
@@ -13,72 +13,65 @@ public class Feladat12 {
         do {
             if (szam < 1) {
                 out.print("Adjon meg egy nagyobb számot: ");
+                szam = sc.nextDouble();
             } else if (szam > 1000) {
                 out.print("Adjon meg egy kisebb számot: ");
-            } else {
-                return;
+                szam = sc.nextDouble();
             }
-            szam = sc.nextDouble();
         } while (szam < 1 || szam > 1000);
-        sc.nextLine();
 
         out.println("A pénz szétosztható a következő módon: ");
 
         double maradek = szam;
 
-        //ötszáz
-        double otszaz = 0;
-        if (szam / 500 > 0) {
-            otszaz = 1;
-            maradek -= 500;
-            out.println("Öszázasok: " + (int) otszaz);
-        }
         //kétszáz
-        double ketszaz = 0;
-        if (szam - (otszaz * 500) >= 200) {
+        double ketszaz;
+        if (maradek >= 200) {
             ketszaz = Math.floor(maradek / 200);
-            maradek -= ketszaz*200;
-            out.println("Kétszázasok: " + (int) ketszaz);
+            maradek -= (ketszaz * 200);
+            out.printf("\nKétszázasok: %.0f",ketszaz);
         }
 
         //száz
-        double szaz = 0;
+        double szaz;
         if (maradek >= 100) {
             szaz = Math.floor(maradek / 100);
-            maradek -= szaz*100;
-            out.println("Százasok: " + (int) szaz);
+            maradek -= (szaz * 100);
+            out.printf("\nSzázasok: %.0f",szaz);
         }
 
         //ötven
-        double otven = 0;
+        double otven;
         if (maradek >= 50) {
             otven = Math.floor(maradek / 50);
-            maradek -= otven*50;
-            out.println("Ötvenesek: " + (int) otven);
+            maradek -= (otven * 50);
+            out.printf("\nÖtvenesek: %.0f",otven);
         }
 
         //husz
-        double husz = 0;
+        double husz;
         if (maradek >= 20) {
             husz = Math.floor(maradek / 20);
-            maradek -= husz*20;
-            out.println("Huszasok: " + (int) husz);
+            maradek -= (husz * 20);
+            out.printf("\nHuszasok: %.0f",husz);
         }
 
         //tiz
-        double tiz = 0;
+        double tiz;
         if (maradek >= 10) {
             tiz = Math.floor(maradek / 10);
-            maradek -= tiz*10;
-            out.println("Tizesek: " + (int) tiz);
+            maradek -= (tiz * 10);
+            out.printf("\nTizesek: %.0f",tiz);
         }
 
         //öt
-        double ot = 0;
-        if (maradek >=5) {
+        double ot;
+        if (maradek >= 5) {
             ot = Math.floor(maradek / 5);
-            maradek -= ot*5;
-            out.println("Ötösök: " + (int) ot);
+            maradek -= (ot * 5);
+            out.printf("\nÖtösök: %.0f",ot);
         }
+
+        out.printf("\nA maradék: %.0f",maradek);
     }
 }
