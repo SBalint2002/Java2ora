2c381d7a04869ab4a7015b3b5404837203569875
Sárándi <sarandi.balint@gmail.com>
2022-09-10 13:31:56 +0200

    Task 15 done

--- src/hu/petrik/java02ora/Feladat15.java
+++ src/hu/petrik/java02ora/Feladat15.java
@@ -1,4 +1,22 @@
 package hu.petrik.java02ora;
 
+import java.util.Random;
+import java.util.Scanner;
+import static java.lang.System.*;
+
 public class Feladat15 {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int low = -100;
+        int high = 100;
+        double[] tomb = new double[25];
+        for (int i = 0; i < 25; i++){
+            tomb[i] = r.nextDouble(high-low)+low;
+        }
+        for (int i = 0; i<25; i++){
+            if (i%2==0){
+                out.printf("%s: %f\n",(i+1),tomb[i]);
+            }
+        }
+    }
 }
